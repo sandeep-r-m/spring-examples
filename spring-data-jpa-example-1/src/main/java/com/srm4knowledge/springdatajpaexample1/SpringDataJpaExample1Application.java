87f676201266c80a4e208f23ab2b7bb18f73cea5
sandeep-r-m <srm4knowledge@gmail.com>
2018-12-16 12:26:23 +0530

    spring-data-jpa-example-1 | Initial Commit
    Signed-off-by: sandeep-r-m <srm4knowledge@gmail.com>

---
+++ spring-data-jpa-example-1/src/main/java/com/srm4knowledge/springdatajpaexample1/SpringDataJpaExample1Application.java
@@ -0,0 +1,14 @@
+package com.srm4knowledge.springdatajpaexample1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaExample1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJpaExample1Application.class, args);
+	}
+
+}
+
